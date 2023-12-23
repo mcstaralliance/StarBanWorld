4f4bb510e69a0bbf1a462e20a46287dc26121037
LDS_XiaoYe <xiaoyecreate@outlook.com>
2023-12-23 12:39:30 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # StarBanWorld
+
+为适配家园插件，基于 1.16.5 的 Bukkit API 重写。
+
+修改自 BanWorld。
---
+++ src/main/java/com/mcstaralliance/starbanworld/StarBanWorld.java
@@ -0,0 +1,61 @@
+package com.mcstaralliance.starbanworld;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public final class StarBanWorld extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        List<String> list = getConfig().getStringList("white-list");
+        Player player = event.getPlayer();
+        String worldName = event.getTo().getWorld().getName();
+        if (worldName.equalsIgnoreCase(player.getName())) {
+            return;
+        }
+        if (list.contains(worldName)) {
+            return;
+        }
+        if (player.hasPermission("world." + worldName)) {
+            return;
+        }
+        String message = ChatColor.RED + "你没有前往" + worldName + "世界的权限";
+        player.sendMessage(message);
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerPortal(PlayerPortalEvent event) {
+        List<String> list = getConfig().getStringList("white-list");
+        Player player = event.getPlayer();
+        String worldName = event.getTo().getWorld().getName();
+        if (worldName.equalsIgnoreCase(player.getName())) {
+            return;
+        }
+        if (list.contains(worldName)) {
+            return;
+        }
+        if (player.hasPermission("world." + worldName)) {
+            return;
+        }
+        String message = ChatColor.RED + "你没有前往" + worldName + "世界的权限";
+        player.sendMessage(message);
+        event.setCancelled(true);
+    }
+
+
+}
